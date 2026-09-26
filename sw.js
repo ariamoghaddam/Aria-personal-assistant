9457a92f510b2052c94d8d3eaa906ccc10c11df0
ariamoghaddam <aria.asghari.m@gmail.com>
2026-09-26 21:27:26 +0330

    Bump ARIA service worker cache for iPhone and iPad parity

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aria-cloud-v27';
+const CACHE='aria-cloud-v36';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))),self.clients.claim()]));});
 self.addEventListener('push',event=>{
