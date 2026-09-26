8ca257904c9739683ae0fe456a06fdf94a85f15b
ariamoghaddam <aria.asghari.m@gmail.com>
2026-09-26 21:51:26 +0330

    Bump ARIA worker to v38

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aria-cloud-v36';
+const CACHE='aria-cloud-v38';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))),self.clients.claim()]));});
 self.addEventListener('push',event=>{
